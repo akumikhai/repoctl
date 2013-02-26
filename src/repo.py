3df747f3bd2f164afb5c69e71470915dfe76a7a0
Mikhail Akulov <akulovmv@gmail.com>
2013-02-26 20:46:19 +0400

    заготовки для необходимых процедур

--- src/repo.py
+++ src/repo.py
@@ -12,23 +12,40 @@ Usage:
     repoctl <command> <options>
 """
 
-def cmd_version():
+def cmd_usage(argd):
+    usage_message()
+
+def cmd_version(argd):
     print "cmd_version"
 
-def cmd_help():
-    print "cmd_help"
+def cmd_help(argd):
+    usage_message()
     
-def cmd_pullall():
+def cmd_pullall(argd):
     print "cmd_pullall"
 
-def cmd_pushall():
+def cmd_pushall(argd):
     print "cmd_pushall"
     
+
+Dcmd = {
+    'usage': cmd_usage,
+    'version': cmd_version,
+    'help': cmd_help,
+    'pullall': cmd_pullall,
+    'pushall': cmd_pushall,
+}
+
 
 ap = cmdlineargs_cfg.mk_argparser()
 
 if ap.parse_args(sys.argv[1:]):
-    print ap.result
+    cmd = ap.result.get('command','usage')
+    fcmd = Dcmd[cmd]
+
+    r = fcmd(ap.result)
+    sys.exit(r)
+    
 else:
     usage_message()
     sys.exit(-1)
